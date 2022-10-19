333421d9cb2573b182bce2fde550c6d80fea5d9f
Your Name <rodrigo.santos1994a@gmail.com>
2022-10-19 20:32:51 -0300

    19/10/2022 User Creation

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, "home.html")
 
 
-def helloworld(request):
-    return render(request, "signup.html", )
+def signup(request):
+
+    if request.method=="GET":
+        
+        return render(request, "signup.html",{
+            "form":UserCreationForm
+        })
+    
+
+    else:
+        if request.POST["password1"]== request.POST["password2"]:
+            try:
+                user = User.objects.create_user(username=request.POST["username"], password=request.POST["password1"])
+                user.save()
+                return HttpResponse("User created")
+            except:
+                return HttpResponse("Username already exists")
+        else:
+            return HttpResponse("Passwords do not match")
