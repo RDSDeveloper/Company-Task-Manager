feeacde1409acb12c70634d75795a207cecc2909
Your Name <rodrigo.santos1994a@gmail.com>
2022-10-19 21:26:47 -0300

    19/10/2022 User creation error covered

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from django.http import HttpResponse
+from django.db import IntegrityError
+from django.contrib.auth import login
 
 
 def home(request):
@@ -22,13 +23,19 @@ def signup(request):
                 user = User.objects.create_user(
                     username=request.POST["username"], password=request.POST["password1"])
                 user.save()
-                return HttpResponse("User created succesfully")
-            except:
+                login(request, user)
+                return redirect("tasks")
+            except IntegrityError:
                 return render(request, "signup.html", {
                     "form": UserCreationForm,
                     "error": "Username already exists"
                 })
+
+            
         return render(request, "signup.html", {
             "form": UserCreationForm,
             "error": "Passwords do not match"
         })
+
+def tasks(request):
+    return render(request, "tasks.html")
